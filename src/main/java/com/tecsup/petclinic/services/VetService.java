da29cdd6fc2555fe10bf2532d1471f50c61acf0b
Vania <sonalysifuentes@gmail.com>
2025-05-29 13:37:04 -0500

    VetService Updated

--- src/main/java/com/tecsup/petclinic/services/VetService.java
+++ src/main/java/com/tecsup/petclinic/services/VetService.java
@@ -8,50 +8,38 @@ import java.util.List;
 public interface VetService {
 
     /**
-     * Crea un nuevo veterinario.
-     *
      * @param vet
-     * @return Vet creado
+     * @return
      */
     Vet create(Vet vet);
 
     /**
-     * Actualiza un veterinario existente.
-     *
      * @param vet
-     * @return Vet actualizado
+     * @return
      */
     Vet update(Vet vet);
 
     /**
-     * Elimina un veterinario por ID.
-     *
      * @param id
      * @throws VetNotFoundException
      */
     void delete(Integer id) throws VetNotFoundException;
 
     /**
-     * Busca un veterinario por ID.
-     *
      * @param id
-     * @return Vet encontrado
+     * @return
      * @throws VetNotFoundException
      */
     Vet findById(Integer id) throws VetNotFoundException;
 
     /**
-     * Busca veterinarios por nombre.
-     *
      * @param firstName
-     * @return Lista de veterinarios que coinciden con el nombre
+     * @return
      */
     List<Vet> findByFirstName(String firstName);
 
     /**
-     * Lista todos los veterinarios.
-     *
-     * @return Lista completa de veterinarios
+     * @return
      */
     List<Vet> findAll();
 }
